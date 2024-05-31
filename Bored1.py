b6a7255832ce6426af90bdf714722341f6c3d288
Arvin Goel <130349816+Arvin-Goel@users.noreply.github.com>
2024-05-31 10:11:02 +0530

    Update Bored1.py

--- Bored1.py
+++ Bored1.py
@@ -1,6 +1,9 @@
-ayb=input(print("Are you bored?(Yes or No)")).lower()
+ayb=input(print("Are you bored?(Yes or No)"))
 while True:
     if ayb="Yes":
         try:
             hb=int(input(print"Yeah, me too. But How Bored?(on a scale of 1 to 10)"))
+            
+print("Wow only (hb)% bored? I am a 100% bored")
+         
     
